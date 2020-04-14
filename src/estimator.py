f7f2f41da29b0290ed905d47adeee98f24e68ab0
Musavili <musavilileon@gmail.com>
2020-04-15 01:50:47 +0300

    covid19estimator

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,99 @@
+import math
+import pprint
+
+data = {"region": {"name": {"Africa"},                             #input details
+                             "avgAge": "19.7",
+                             "avgDailyIncomeInUSD": "5",
+                             "avgDailyIncomePopulation": "0.71"
+                              },
+                    "periodType": "days",     #subject to modification
+                    "timetoElapse": "58",               #subject to change
+                    "reportedCases": "674",              #subject to change
+                    "population": "66622705",
+                    "totalHospitalBeds": "1380614"
+                    }
+
 def estimator(data):
-  return data
+    data = {"name": {input()},  # input details
+                       "avgAge": input(),
+                       "avgDailyIncomeInUSD": input(),
+                       "avgDailyIncomePopulation": input(),
+
+            "periodType": input(),  # subject to modification
+            "timetoElapse": input(),  # subject to change
+            "reportedCases": input(),  # subject to change
+            "population": input(),
+            "totalHospitalBeds": input()
+            }
+
+    return data
+
+
+def impactEstimator():
+    timeToElapse = int(estimator(data)["timetoElapse"])
+    severityFactor = int(timeToElapse) / 3
+    impactFactor = int(timeToElapse / 3)
+    reportedCases = int(estimator(data)['reportedCases'])
+    currentlyInfected = int(reportedCases * 10)
+    infectionsRequest = int(currentlyInfected) * (2 * math.pow(2, severityFactor))
+    severeCasesRequest = int(infectionsRequest) * 0.15
+    availableBeds = int(estimator(data)["totalHospitalBeds"]) * 0.35
+
+    impact= {
+        "currentlyInfected": str(currentlyInfected),   #subject to change
+        "infectionsByRequestTime": str(currentlyInfected * (2 * math.pow(2,impactFactor))),
+        "severeCasesByRequestedTime":  str(severeCasesRequest),
+        "hospitalBedsByRequestTime":str(availableBeds - severeCasesRequest),
+        "casesForICUByRequestTime":  str(infectionsRequest * 0.05),
+        "casesForVentilatorsByRequestTime":str(infectionsRequest * 0.02),
+        "dollarsInFlight": str(int(infectionsRequest) * int(estimator(data)["avgDailyIncomePopulation"]) * int(estimator(data)["avgDailyIncomeInUSD"]))
+    }
+
+    return impact
+
+def severityEstimator():
+    timeToElapse = int(estimator(data)["timetoElapse"])
+    severityFactor = int(timeToElapse)/3
+    availableBeds = int(estimator(data)["totalHospitalBeds"]) * 0.35
+    reportedCases = int(estimator(data)['reportedCases'])
+    currentlyInfected = int(reportedCases * 50)
+    infectionsRequest = int(currentlyInfected) * (2 * math.pow(2, severityFactor))
+    severeCasesRequest = int(infectionsRequest) * 0.15
+
+    severeImpact = {
+        "currentlyInfected": str(currentlyInfected),
+        "infectionsByRequestedTime": str(infectionsRequest),
+        "severeCasesByRequestedTime": str(severeCasesRequest) ,
+        "hospitalBedsByRequestTime": str(availableBeds - severeCasesRequest),
+        "casesForICUByRequestTime": str(infectionsRequest * 0.05),
+        "casesForVentilatorsByRequestTime": str(infectionsRequest * 0.02),
+        "dollarsInFlight": str(int(infectionsRequest) * int(estimator(data)["avgDailyIncomePopulation"]) * int(estimator(data)["avgDailyIncomeInUSD"]))
+    }
+
+    return severeImpact
+
+def convert():
+    if data["periodType"] == "weeks": #SUBJECT TO CHANGE
+        weeksToDays()
+    elif data["periodType"] == "months": #subject to change
+        monthsToDays()
+
+def weeksToDays():
+    estimator(data)
+    data["periodType"].set(int(data["periodType"].get())*7)
+
+def monthsToDays():
+    estimator(data)
+    data["periodType"].set(int(data["periodType"].get())*30)
+
+def display():
+    from pprint import pprint as pp
+    print("input data in the following order:")
+    print(data)
+    pp(estimator(data))
+    pp(severityEstimator())
+    pp(impactEstimator())
+    exit(1)
+
+if __name__=='__main__':
+    display()
